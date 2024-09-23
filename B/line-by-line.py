bcaf242da97fa88057cb9ad3f9bd16b5de20ec00
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-09-23 11:35:43 +0500

    Auto commit on save

--- B/line-by-line.py
+++ B/line-by-line.py
@@ -37,5 +37,5 @@ def process_file(input_filename, output_filename):
 
 # File with the input data
 input_filename = 'line_by_line_input.txt'
-output_filename = 'line_by_line_output.txt'
+output_filename = 'output.txt'
 process_file(input_filename, output_filename)

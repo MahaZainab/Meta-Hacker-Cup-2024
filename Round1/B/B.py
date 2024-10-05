fd3e2c86ac894df46fdf0b46bf1710432746eb31
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-10-06 00:53:33 +0500

    Auto commit on save

--- Round1/B/B.py
+++ Round1/B/B.py
@@ -37,7 +37,7 @@ def process_file(filename):
     
     # Print all results
     for i in range(1, len(results) + 1):
-        print("Case #" + str(i) + ": " str(results[i]))
+        print("Case #" + str(i) + ": " str(results[i-1]))
 # File with the input data
 filename = 'prime_subtractorization_sample_input.txt'
 # filename = "prime_subtractorization_validation_input.txt"
